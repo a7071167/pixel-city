a0b34c9f70b6f9fffb30d6254cef517d8a6bb1e8
Alexander Kufel <kufelalexander@gmail.com>
2018-08-14 07:59:14 +0300

    Start point

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by user on 14.08.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
